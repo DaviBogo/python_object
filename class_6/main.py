6a750f42f6653806b63003f66bb4d375502fc2d3
Davi Bogo <bogodavi10@gmail.com>
2022-07-23 19:12:44 -0300

    aggregation

---
+++ class_6/main.py
@@ -0,0 +1,13 @@
+from objects import ShoppingCart, Product
+
+cart = ShoppingCart()
+
+p1 = Product('Shirt', 50)
+p2 = Product('Iphone', 10000)
+p3 = Product('Mug', 15)
+
+cart.add_product(p1)
+cart.add_product(p2)
+cart.add_product(p3)
+cart.list_products()
+print(cart.total_price())
