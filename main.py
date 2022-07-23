4111f2d0d12326570bf541e53df94ad16c2623d9
Davi Bogo <bogodavi10@gmail.com>
2022-07-23 15:37:13 -0300

    first class

---
+++ main.py
@@ -0,0 +1,6 @@
+from person import Person
+
+p1 = Person('Davi', 22)
+p2 = Person('Érica', 21)
+
+print(p1.get_birth_date())
---
+++ person.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from unicodedata import name
+
+
+class Person:
+    current_year = int(datetime.strftime(datetime.now(), '%Y'))
+
+    def __init__(self, name, age, eating=False, speaking=False):
+        self.name = name
+        self.age = age
+        self.eating = eating
+        self.speaking = speaking
+
+    def speak(self, subject):
+        if self.eating:
+            print(f"{self.name} can't speak while eating")
+            return
+        if self.speaking:
+            print(f'{self.name} already speaking')
+
+        print(f'{self.name} is speaking about {subject}')
+        self.speaking = True
+
+    def stop_speaking(self):
+        if not self.speaking:
+            print(f'{self.name} is already not speaking')
+            return
+
+        print(f'{self.name} stopped speaking...')
+        self.speaking = False
+
+    def eat(self, food):
+        if self.eating:
+            print(f'{self.name} already eating')
+            return
+        if self.speaking:
+            print(f"{self.name} can't eat while speaking")
+            return
+
+        print(f'{self.name} is eating {food}...')
+        self.eating = True
+
+    def stop_eating(self):
+        if not self.eating:
+            print(f'{self.name} is already not eating')
+            return
+
+        print(f'{self.name} stopped eating...')
+        self.eating = False
+
+    def get_birth_date(self):
+        return self.current_year - self.age
