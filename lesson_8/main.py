7237a4ea44d63edeee192ba4c68e7bdadf55958a
Davi Bogo <bogodavi10@gmail.com>
2022-07-24 22:07:36 -0300

    heritage

--- lesson_8/main.py
+++ lesson_8/main.py
@@ -8,3 +8,8 @@ client = Client('Davi', 22)
 client.speak()
 print(client.class_name)
 client.buy()
+
+print()
+
+client_vip = ClientVIP('Érica', 21, 'Scherner')
+client_vip.speak()
--- lesson_8/objects.py
+++ lesson_8/objects.py
@@ -3,16 +3,26 @@ class Person:
         self.name = name
         self.age = age
         self.class_name = self.__class__.__name__
-    
+
     def speak(self):
-        print(f'{self.name} Speaking...')
+        print(f'{self.class_name} Speaking...')
 
 
 class Client(Person):
     def buy(self):
-        print(f'{self.name} is buying...')
+        print(f'{self.class_name} is buying...')
+
+
+class ClientVIP(Client):
+    def __init__(self, name, age, last_name):
+        super().__init__(name, age)
+        self.last_name = last_name
+
+    def speak(self):
+        print(f'{self.name} is VIP')
+        super().speak()
 
 
 class Student(Person):
     def study(self):
-        print(f'{self.name} is studying...')
+        print(f'{self.class_name} is studying...')
